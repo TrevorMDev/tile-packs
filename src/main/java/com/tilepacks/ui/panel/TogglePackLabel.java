704e10891abb4b40cd4c035d5917cf15aa642ac4
Trevor M <9042126+TrevorMDev@users.noreply.github.com>
2024-07-12 01:59:30 +1200

    fix minus link

--- src/main/java/com/tilepacks/ui/panel/TogglePackLabel.java
+++ src/main/java/com/tilepacks/ui/panel/TogglePackLabel.java
@@ -55,7 +55,7 @@ public class TogglePackLabel extends JLabel {
         final BufferedImage addIcon = ImageUtil.loadImageResource(TilePacksPlugin.class, "add_icon.png");
         ADD_ICON = new ImageIcon(addIcon);
         ADD_ICON_HOVER = new ImageIcon(ImageUtil.alphaOffset(addIcon, 0.53f));
-        // Icon is https://www.flaticon.com/free-icon/minus_1143362
+        // Icon is https://www.flaticon.com/free-icon/minus_11433623
         // Made by https://www.flaticon.com/authors/utari-nuraeni
         final BufferedImage removeIcon = ImageUtil.loadImageResource(TilePacksPlugin.class, "remove_icon.png");
         REMOVE_ICON = new ImageIcon(removeIcon);
